b25c85ad9a59d16f62d05951d4b097af780131a5
Alexander Vereta <alexander.git@yandex.ru>
2025-04-02 22:32:59 +0500

    Initial commit

---
+++ Antlr4VS/Program.cs
@@ -0,0 +1,22 @@
+﻿using Antlr4.Runtime;
+
+namespace Antlr4VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "int a = 5;";
+            ICharStream inputStream = CharStreams.fromString(input);
+            CSharpLexer lexer = new(inputStream);
+            CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+            CSharpParser parser = new(commonTokenStream);
+            CSharpParser.ExpressionContext expressionContext = parser.expression();
+
+
+            ICSharpParserVisitor<string> visitor = new CSharpParserBaseVisitor<string>();
+
+            Console.WriteLine(visitor.Visit(expressionContext));
+        }
+    }
+}
